74e7933ab810762fb9e8ce46cccca4f8b49e8325
casolaroSalvatore <150945794+casolaroSalvatore@users.noreply.github.com>
2025-07-06 21:18:38 +0200

    Update DBConnectionTest.java

--- src/test/DBConnectionTest.java
+++ src/test/DBConnectionTest.java
@@ -1,32 +1,28 @@
 package test;
 
 import logic.model.dao.db.ConnectionFactory;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 import java.sql.Connection;
 import java.sql.SQLException;
-import static org.testng.AssertJUnit.assertEquals;
-import static org.testng.AssertJUnit.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-/* Verifica rapida sull’integrità della connessione JDBC ottenuta da ConnectionFactory */
 class DBConnectionTest {
 
     private final Connection conn = ConnectionFactory.getConnection();
 
     @Test
     void testGetConnection() {
-        assertNotNull(conn);
+        assertNotNull(conn, "Connection should not be null");
     }
 
     @Test
     void testIsClosed() throws SQLException {
-        assertEquals(false, conn.isClosed());
+        assertEquals(false, conn.isClosed(), "Connection should be open");
     }
 
     @Test
     void testIsValid() throws SQLException {
-        assertEquals(true, conn.isValid(2));
+        assertEquals(true, conn.isValid(2), "Connection should be valid");
     }
 }
-
-
-
