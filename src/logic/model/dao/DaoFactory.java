9bc45b042253f4a64ac057583f0d6c39c1e509eb
casolaroSalvatore <150945794+casolaroSalvatore@users.noreply.github.com>
2025-07-08 17:07:27 +0200

    Add files via upload

--- src/logic/model/dao/DaoFactory.java
+++ src/logic/model/dao/DaoFactory.java
@@ -4,7 +4,6 @@ import logic.exception.NoImplementationForPersistenceProviderException;
 
 import java.lang.reflect.InvocationTargetException;
 
-@SuppressWarnings("java:S7027")
 // Factory astratta per fornire le DAO in base al PersistenceProvider scelto
 public abstract class DaoFactory {
 
