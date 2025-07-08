d602966e0ca413459229f81616c1c56a2ab67ae9
casolaroSalvatore <150945794+casolaroSalvatore@users.noreply.github.com>
2025-07-08 17:30:19 +0200

    Delete model directory

--- model/domain/state/RefusedState.java
+++
@@ -1,7 +0,0 @@
-package logic.model.domain.state;
-
-// ConcreteState che rappresenta una sessione rifiutata dal tutor.
-// Si tratta di uno stato terminale: non sono previste transizioni successive da qui.
-class RefusedState extends AbstractTutoringSessionState {
-    // Nessun metodo è sovrascritto: lo stato è terminale e non consente ulteriori eventi o transizioni.
-}
