0aceabf4889c94891007966dd7afcfce232f5a05
casolaroSalvatore <150945794+casolaroSalvatore@users.noreply.github.com>
2025-07-08 08:38:19 +0200

    Delete model directory

--- model/domain/InterfaceProvider.java
+++
@@ -1,6 +0,0 @@
-package logic.model.domain;
-
-// Enum che rappresenta i tipi di interfaccia utente supportati dall'applicazione: Colored e BW (CLI)
-public enum InterfaceProvider {
-    COLORED, BW;
-}
--- model/domain/ReviewStatus.java
+++
@@ -1,9 +0,0 @@
-package logic.model.domain;
-
-// Enum che rappresenta lo stato di una recensione in una sessione
-// di tutoraggio: non iniziata, pendente, completata
-public enum ReviewStatus {
-    NOT_STARTED,
-    PENDING,
-    COMPLETE
-}
